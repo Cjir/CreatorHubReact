f81d571fadb7e6729f05febd908c06db3117351f
Christopher Roman <chrisjiroman@gmail.com>
2020-05-11 22:00:36 -0400

    Directory Component and shared file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
+import { CREATORHUB } from './shared/creatorhub'
 
 class App extends Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            creatorhub: CREATORHUB
+        }
+    }
     render() {
         return (
             <div className="App">
                 <Navbar>
                 <div className="container">
-                    <Directory />
+                    <Directory creatorhub={this.state.creatorhub} />
                 </div>
                 </Navbar>
             </div>
---
+++ src/shared/creatorhub.js
@@ -0,0 +1,8 @@
+export const CREATORHUB = [
+        {
+            id: 0,
+            name: 'Creator Hub Logo',
+            image: 'assets/images/creatorhublt.png',
+            description: 'Marketplace, blogg, a hub for Creation'
+        }
+]
